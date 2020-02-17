27d9dd2c68c8ce162d9e840e20e14acb9844d7ad
Ahish9009 <ahishjayant9009@gmail.com>
2020-02-17 22:03:17 +0530

    Added more instructions

---
+++ execute/src/decode_instr.v
@@ -0,0 +1,208 @@
+// instructions implemented
+    
+// |----------------------------------------------|
+// |S.No| Instruction   | Type | OPCODE |  FUNCT  |
+// |----------------------------------------------|
+// |  1 |     ADD       |  R   | 000000 | 100000  |
+// |  2 |     AND       |  R   | 000000 | 100100  |
+// |  3 |     NOR       |  R   | 000000 | 100111  |
+// |  4 |     OR        |  R   | 000000 | 100101  |
+// |  5 |     SUB       |  R   | 000000 | 100010  |
+// |  6 |     XOR       |  R   | 000000 | 100110  |
+// |  7 |     SLT       |  R   | 000000 | 101010  |
+// |  8 |     SRA       |  R   | 000000 | 000011  |
+// |  9 |     SRAV      |  R   | 000000 | 000111  |
+// | 10 |     SRL       |  R   | 000000 | 000010  |
+// | 11 |     SRLV      |  R   | 000000 | 000110  |
+// | 12 |     SLL       |  R   | 000000 | 000000  |
+
+
+// | 11 |     ADDI      |  I   | 001000 | 000000  |
+// | 11 |     ANDI      |  I   | 001100 | 000000  |
+// | 11 |     XORI      |  I   | 001110 | 000000  |
+// | 11 |     ORI       |  I   | 001101 | 000000  |
+
+
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:10:21 02/02/2020 
+// Design Name: 
+// Module Name:    decode_instr 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module decode_instr(
+    input [31:0] instr,
+    output [4:0] shamt,
+    output [25:0] instr_index,
+    output [15:0] imm16,
+    output reg_dst,
+    output reg_write,
+    output mem_read,
+    output mem_write,
+    output jump,
+    output branch,
+    output sign_ext,
+    output alu_src,
+    output [3:0] alu_ctr,
+    output [4:0] rs,
+    output [4:0] rt,
+    output [4:0] rd
+    );
+	 
+	wire [5:0] opcode, funct;
+//	wire [4:0] shamt;
+//	wire [15:0] imm16;
+//	wire [25:0] instr_index;
+	 
+	assign opcode = instr[31:26];
+	assign rs = instr[25:21];
+ 	assign rt = instr[20:16];
+	assign rd = instr[15:11];
+	assign shamt = instr[10:6];
+	assign funct = instr[5:0];
+	assign imm16 = instr[15:0];
+	assign instr_index = instr[25:0];
+	
+	//decides which operation ALU should perform
+	reg [3:0] alu_ctr_reg;
+	assign alu_ctr = alu_ctr_reg;
+	
+	// all these can be decided simply by opcode
+	assign reg_dst = (opcode == 6'b000000) ? 1 : 0; 
+	assign reg_write = (opcode == 6'b000000 | opcode == 6'b001000 | opcode == 6'b001100 | opcode == 6'b001110 | opcode == 6'b001101 | opcode == 6'b10011) ? 1 : 0;
+	assign mem_read = (opcode == 6'b100011) ? 1 : 0;
+	assign mem_write = (opcode == 6'b101011) ? 1 : 0;
+	assign jump = (opcode == 6'b000010) ? 1 : 0;
+	assign branch = (opcode == 6'b000100) ? 1 : 0;	
+	assign sign_ext = (opcode == 6'b001000 | opcode == 6'b101011 | opcode == 6'b100011) ? 1 : 0; 
+	assign alu_src = (opcode == 6'b000000 | opcode == 6'b000100) ? 0 : 1;
+	
+	always @(*) begin
+		
+		// if it is a R operation
+		if (opcode == 6'b000000) begin
+
+			//add
+			if (funct == 6'b100000 | funct == 6'b100001) begin // ADD, 
+				alu_ctr_reg = 4'b0000;
+			end
+			//and
+			if (funct == 6'b100100) begin // AND,
+			    alu_ctr_reg = 4'b1001;
+			end
+			//or
+			if (funct == 6'b100101) begin // OR
+				alu_ctr_reg = 4'b1010;
+			end
+			//nor
+			if (funct == 6'b100111) begin // NOR
+				alu_ctr_reg = 4'b1100;
+			end
+			//sub
+			if (funct == 6'b100010) begin // SUB                              
+				alu_ctr_reg = 4'b0001;
+			end
+			//xor
+			if (funct == 6'b100110) begin // XOR                            
+				alu_ctr_reg = 4'b1101;
+			end
+			//less than
+			if (funct == 6'b101010) begin // SLT
+			    alu_ctr_reg = 4'b0111;
+			end
+			//Shift-R-S-RS
+			if (funct == 6'b000111) begin // SRAV
+			    alu_ctr_reg = 4'b0011;
+			end
+			//Shift-R-S-SHAMT
+			if (funct == 6'b000011) begin // SRA
+			    alu_ctr_reg = 4'b1011;
+			end
+			//Shift-R-US-RS
+			if (funct == 6'b000010) begin // SRLV
+			    alu_ctr_reg = 4'b1110;
+			end
+			//Shift-R-US-SHAMT
+			if (funct == 6'b000110) begin // SRL
+			    alu_ctr_reg = 4'b1111;
+			end
+			//Shift-L-RS
+			if (funct == 6'b000100) begin // SLLV
+			    alu_ctr_reg = 4'b0100;
+			end
+			//Shift-L-SHAMT
+			if (funct == 6'b000000) begin // SLL
+			    alu_ctr_reg = 4'b0101;
+			end
+			
+			//Shift-L-SHAMT
+			
+		end
+		// if it is an I operation
+		else begin
+			case(opcode) 
+				
+				//ADDI
+				6'b001000:
+				begin
+					alu_ctr_reg = 4'b0000; //add
+				end
+				
+				//ANDI
+				6'b001100:
+				begin
+					alu_ctr_reg = 4'b1001; //and
+				end
+				
+				//XORI
+				6'b001110:
+				begin
+					alu_ctr_reg = 4'b1101; //xor
+				end
+				
+				//ORI
+				6'b001101:
+				begin
+					alu_ctr_reg = 4'b1010; //or
+				end
+				
+				//BEQ
+				6'b000100:
+				begin
+					alu_ctr_reg = 4'b0001; //sub
+				end
+				
+				//LW
+				6'b100011:
+				begin
+					alu_ctr_reg = 4'b0000; //add
+				end
+				
+				//SW
+				6'b101011:
+				begin
+					alu_ctr_reg = 4'b0000; //add
+				end
+			endcase
+		end
+			
+		
+	end
+
+
+endmodule
+
---
+++ execute/src/execute_unit.v
@@ -0,0 +1,159 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.02.2020 11:22:08
+// Design Name: 
+// Module Name: execute_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module execute_unit(
+    input CLK, 
+    input [31:0] instr,
+    output [31:0] busA, //have to show the full array here
+    output [31:0] busB,
+    output [31:0] busW,
+	 output [31:0] reg0,
+	 output [31:0] reg1,
+	 output [31:0] reg2,
+	 output [31:0] reg3,
+	 output [31:0] reg4,
+	 output [31:0] reg5,
+	 output [31:0] reg6,
+	 output [31:0] reg7,
+	 output [31:0] reg8,
+	 output [31:0] reg9,
+	 output [31:0] reg10,
+	 output [31:0] reg11,
+	 output [31:0] reg12,
+	 output [31:0] reg13,
+	 output [31:0] reg14,
+	 output [31:0] reg15,
+	 output [31:0] reg16,
+	 output [31:0] reg17,
+	 output [31:0] reg18,
+	 output [31:0] reg19,
+	 output [31:0] reg20,
+	 output [31:0] reg21,
+	 output [31:0] reg22,
+	 output [31:0] reg23,
+	 output [31:0] reg24,
+	 output [31:0] reg25,
+	 output [31:0] reg26,
+	 output [31:0] reg27,
+	 output [31:0] reg29,
+	 output [31:0] reg30,
+	 output [31:0] reg31
+    );
+/*
+		initializing registers
+*/
+	 reg [31:0] regs [31:0]; //initializes the array
+	 
+	 //sets all regs to 0
+	 integer i;
+    initial begin 
+       for (i = 0; i < 32; i=i+1) begin
+            regs[i] = 4;
+       end
+    end
+//---------------------------------------------------------------------
+	 
+/*
+		for output
+*/
+	 assign reg0 = regs[0];
+	 assign reg1 = regs[1];
+	 assign reg2 = regs[2];
+	 assign reg3 = regs[3];
+	 assign reg4 = regs[4];
+	 assign reg5 = regs[5];
+	 assign reg6 = regs[6];
+	 assign reg7 = regs[7];
+	 assign reg8 = regs[8];
+	 assign reg9 = regs[9];
+	 assign reg10 = regs[10];
+	 assign reg11 = regs[11];
+	 assign reg12 = regs[12];
+	 assign reg13 = regs[13];
+	 assign reg14 = regs[14];
+	 assign reg15 = regs[15];
+	 assign reg16 = regs[16];
+	 assign reg17 = regs[17];
+	 assign reg18 = regs[18];
+	 assign reg19 = regs[19];
+	 assign reg20 = regs[20];
+	 assign reg21 = regs[21];
+	 assign reg22 = regs[22];
+	 assign reg23 = regs[23];
+	 assign reg24 = regs[24];
+	 assign reg25 = regs[25];
+	 assign reg26 = regs[26];
+	 assign reg27 = regs[27];
+	 assign reg28 = regs[28];
+	 assign reg29 = regs[29];
+	 assign reg30 = regs[30];
+	 assign reg31 = regs[31];	 
+//---------------------------------------------------------------------
+    
+/*
+		for decoding current instruction
+*/
+    wire [25:0] instr_index;
+    wire [15:0] imm16;
+    wire reg_dst, reg_write, mem_read, mem_write, jump, branch, sign_ext, alu_src;
+    wire [3:0] alu_ctr;
+    wire [4:0] rs, rt, rd, shamt;
+    decode_instr A(instr, shamt, instr_index, imm16, reg_dst, reg_write, mem_read, mem_write, jump, branch, sign_ext, alu_src, alu_ctr, rs, rt, rd);
+//---------------------------------------------------------------------
+	 
+/*
+		for updating registers
+*/
+    wire [4:0] rw, ra, rb;
+
+	 //redundant but follows block diagram
+ 	 assign ra = rs;
+	 assign rb = rt;
+	 //gets the register where write will take place
+    mux2x1_5 M1(reg_dst, rt, rd, rw);
+	 //loads the values onto the buses
+	 assign busA = regs[ra];
+    assign busB = regs[rb];
+   
+    always @(negedge CLK) begin    
+        if (reg_write == 1'b1) begin
+            regs[rw] = busW;
+        end     
+    end
+//---------------------------------------------------------------------
+	 
+/*
+		sign extender
+*/
+    wire [31:0] imm32;
+    extender EXT(sign_ext, imm16, imm32);
+//---------------------------------------------------------------------
+    
+/*
+    alu
+*/
+    wire v, c_out, zero;
+    wire [31:0] alu_in2;
+    mux2x1_32 M2(alu_src, busB, imm32, alu_in2);
+    alu ALU1(alu_ctr, busA, alu_in2, shamt, busW, v, c_out, zero); //output rooted directly to busW as data memory has not been made yet
+//---------------------------------------------------------------------    
+endmodule
+
