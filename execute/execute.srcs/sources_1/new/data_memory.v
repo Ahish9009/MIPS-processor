e211182e0c0c6dc32709127873fa4cdd8988605c
Ahish9009 <ahishjayant9009@gmail.com>
2020-02-20 19:21:56 +0530

    Exec stage mostly done, RAM has to be integrated

---
+++ execute/execute.srcs/sources_1/new/data_memory.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.02.2020 11:23:00
+// Design Name: 
+// Module Name: data_memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module data_memory(
+    input CLK,
+    input write_mem,
+    input [31:0] addr,
+    input [31:0] data_in,
+    output reg [31:0] data_out
+    );
+    
+    reg [31:0] data_memory_array [255:0];
+    
+    //initialize
+    integer j;
+    initial begin
+        for (j=0; j < 256; j=j+1) begin
+            data_memory_array[j] = 31'b0;
+        end
+    end
+    
+    always @(negedge CLK) begin
+        if (write_mem == 1'b1) begin
+            data_memory_array[addr] = data_in;   
+        end
+        
+        data_out <= data_memory_array[addr];
+         
+    end
+    
+    
+    
+endmodule
